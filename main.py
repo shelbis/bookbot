1dd4f85b76e294856f55e8f7ab0b4e8a86c9c3a9
Shelbis <mantvydas970@gmail.com>
2025-11-07 15:08:06 +0100

    Add stats.py and refactoring code

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def get_book_text(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-    return file_contents
-
-def word_count():
-    words = get_book_text("./books/frankenstein.txt").split()
-    total_words = len(words)
-    text = f"Found {total_words} total words"
-    return text
+from stats import word_count
 
 def main():
     print(word_count())
