689f77c8c143d484f1faddf338de53009fc8ebe1
Shelbis <mantvydas970@gmail.com>
2025-11-07 18:49:02 +0100

    Update main.py and stats.py to output character count, add __pycache__ to .gitignore, deleted __pycache__

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import word_count
+from stats import count_characters
 
 def main():
     print(word_count())
-
+    print(count_characters())
+    
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,17 @@ def word_count():
     total_words = len(words)
     text = f"Found {total_words} total words"
     return text
+
+def count_characters():
+    count = 1
+    dictionary = {}
+    text = get_book_text("./books/frankenstein.txt")
+    for letter in text:
+        letter = letter.lower()
+        if letter not in dictionary:
+            dictionary[letter] = count
+        else:
+            current_count = dictionary[letter]
+            current_count += 1
+            dictionary[letter] = current_count
+    return dictionary
